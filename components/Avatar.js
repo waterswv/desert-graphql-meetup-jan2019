0cebc97871e96498490d17716b2627c0eb1e4de8
Waterswv <bmierke@gmail.com>
2019-01-29 17:02:13 -0700

    Fix broken images

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,5 +3,5 @@ import styled from 'styled-components'
 export default styled.img`
   box-shadow: 0 0 1px 11px rgba(21,19,39,0.15), 0 0 1px 22px rgba(21,19,39,0.1);
   border-radius: 50%;
-  width: 150px;
+  width: 175px;
   margin-bottom: 12px;`
